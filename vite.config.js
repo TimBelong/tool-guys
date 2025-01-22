58d365f31d2c8133eafee92188ffe1f533a2c3cc
inne <mrbelongtim@gmail.com>
2025-01-22 16:58:02 +0100

    fixes

--- vite.config.js
+++ vite.config.js
@@ -5,29 +5,12 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['public/assets/sass/main.scss', 'public/assets/js/main.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/assets/sass/main.scss'
+            ],
             refresh: true,
         }),
     ],
-    resolve: {
-        alias: {
-            '~': path.resolve(__dirname, 'public/assets'),
-        },
-    },
-    optimizeDeps: {
-        include: ['axios'],
-    },
-    build: {
-        rollupOptions: {
-            output: {
-                assetFileNames: (assetInfo) => {
-                    if (assetInfo.name && assetInfo.name.endsWith('.css')) {
-                        
-                        return 'assets/css/[name][extname]';
-                    }
-                    return 'assets/[name][extname]';
-                },
-            },
-        },
-    },
 });
